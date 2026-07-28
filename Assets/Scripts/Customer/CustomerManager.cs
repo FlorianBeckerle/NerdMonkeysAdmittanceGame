6919ae7185d389ccb311ef2f39aa77f8d32d22fd
FB <43820946+FlorianBeckerle@users.noreply.github.com>
2026-07-28 15:55:10 +0200

    Added Third Party Assets for Decoration

--- Assets/Scripts/Customer/CustomerManager.cs
+++ Assets/Scripts/Customer/CustomerManager.cs
@@ -220,6 +220,8 @@ public class CustomerManager : MonoBehaviour
             PlayCashSound();
             
             customerActive = false;
+            
+            ordersFullfilled++;//increase amount of finished orders
 
             GiveReward();
         }
